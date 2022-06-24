6768cd59b458aa44cf170f7627e7bacc47a2ad68
Gilbert Ng'eno <ngenogilbert07@gmail.com>
2022-06-24 10:10:19 +0300

    product api

--- src/main/java/com/gilly7/ecommerce/dto/product/ProductDto.java
+++ src/main/java/com/gilly7/ecommerce/dto/product/ProductDto.java
@@ -1,7 +1,10 @@
 package com.gilly7.ecommerce.dto.product;
 
-import javax.validation.constraints.NotNull;
+import com.gilly7.ecommerce.model.Product;
+import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+@Data
 public class ProductDto {
 
     private Integer id;
@@ -10,4 +13,23 @@ public class ProductDto {
     private @NotNull double price;
     private @NotNull String description;
     private @NotNull Integer categoryId;
+
+    public ProductDto(Integer id, String name, String imageURL, double price, String description, Integer categoryId) {
+        this.id = id;
+        this.name = name;
+        this.imageURL = imageURL;
+        this.price = price;
+        this.description = description;
+        this.categoryId = categoryId;
+    }
+
+
+    public ProductDto(Product product) {
+        this.setId(product.getId());
+        this.setName(product.getName());
+        this.setImageURL(product.getImageURL());
+        this.setDescription(product.getDescription());
+        this.setPrice(product.getPrice());
+        this.setCategoryId(product.getCategory().getId());
+    }
 }
